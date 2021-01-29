58a1e79939c7985a9afd639f56fa01df2acb248e
xabier <xabier.fernandez@outlook.com>
2021-01-29 21:30:46 +0100

    añadido modulo medio_sumador

---
+++ medio_sumador.v
@@ -0,0 +1,10 @@
+//Modulo de descripcion del circuito medio sumador
+//Usando puertas logicas
+module medio_sumador(
+    input A,
+    input B,
+    output S,
+    output Cout);
+        xor (S,A,B);
+        and (Cout,A,B);
+endmodule
