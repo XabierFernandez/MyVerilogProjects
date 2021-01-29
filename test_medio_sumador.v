a4f3bd8e75f41ffff0fee4422d616f032d55c3ba
xabier <xabier.fernandez@outlook.com>
2021-01-29 21:52:51 +0100

    añadido modulo de estimulo test_medio_sumador

---
+++ test_medio_sumador.v
@@ -0,0 +1,16 @@
+//Modulo de estimulo para el circuito medio_sumador
+module test_medio_sumador();
+    reg A,B;
+    wire S,Cout;
+    medio_sumador(A,B,S,Cout);
+initial
+    begin
+        A = 1'b0; B = 1'b0;
+        #25 A = 1'b0; B = 1'b1;
+        #25 A = 1'b1; B = 1'b0;
+        #25 A = 1'b1; B = 1'b1;
+        #25;
+    end
+initial
+#200 $finish; //Duracion de la simulacion
+endmodule
