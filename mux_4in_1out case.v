e34f0df6c359eaf5e65b052b3ab6ac335a681d90
Xabier Fernandez <xabier.fernandez@outlook.com>
2021-02-05 17:28:30 +0100

    multiplexor 4 input a 1 output con CASE añadido

---
+++ mux_4in_1out case.v
@@ -0,0 +1,16 @@
+`timescale 1ns/10ps
+//Modulo de descripcion del 
+//circuito multiplexor 4 entradas a 1 salida
+module mux_4in_1out(
+    input  [3:0] I,
+    input [1:0] sel,
+    output reg Q);
+        always @ (sel[1],sel[0],I[0],I[1],I[2],I[3]);
+            case (sel)
+                2'b00 : Q = I[0];
+                2'b01 : Q = I[1];
+                2'b10 : Q = I[2];
+                2'b11 : Q = I[3];
+            endcase
+endmodule
+
